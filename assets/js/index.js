30498a86b1df0e5609cd6f4328196530d67df274
Termins55 <kevgame123@gmail.com>
2025-08-31 17:34:19 +0300

    Add promises base

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,44 @@
 "use strict";
 
-//JSON
+// Promise
 
-const obj = {
-  number: 1,
-  string: "string",
-  array: [1, 2, false],
-  boolean: true,
-  null: null,
-  plainObject: { prop: 10 },
-  // метод не => JSON
-  getNumber() {
-    return this.number;
-  },
-  // undefined не => JSON
-  undefined: undefined,
-};
+// cb виконається через 1000мс
+// setInterval(cb, 1000);
 
-// obj => JSON серіалізація
-const obfJson = JSON.stringify(obj);
+// cb виконається, коли буде подія click
+// button.addEventListener('click', cb)
 
-// JSON => obj десеріалізація
-const parseObj = JSON.parse(obfJson);
+// Колбек, вказаний в then виконається тоді,
+// коли проміс state стане fullfilled
+// Колбек, вказаний в catch виконається тоді,
+// коли проміс state стане rejected
+
+const weatherUrl =
+  "https://api.open-meteo.com/v1/forecast?latitude=48.5083&longitude=32.2662&hourly=temperature_2m&timezone=Europe%2FBerlin&forecast_days=1";
+
+fetch(weatherUrl)
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((err) => console.log("error :>> ", err));
+
+// Second then form:
+// fetch(weatherUrl)
+//   .then((response) => response.json())
+//   .then(
+//     (data) => console.log(data),
+//     (err) => console.log("error :>> ", err)
+//   );
+// Promise state (pending, fullfilled, rejected)
+//         result           payload     error
+console.log("next line");
+
+// callback hell:
+// setTimeout(() => {
+//   setTimeout(() => {
+//     setTimeout(() => {
+//       setTimeout(() => {
+//         setTimeout(() => {}, 2000);
+//       }, 2000);
+//     }, 2000);
+//   }, 2000);
+// }, 1000);
