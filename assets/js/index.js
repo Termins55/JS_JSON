0c1b3534f297d6152b5e8633d8836d2f7b633373
Termins55 <kevgame123@gmail.com>
2025-09-02 20:06:24 +0300

    Add weather example

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,40 @@
 "use strict";
 
-alert('qwerty'); // блокування - погано!
+const weatherUrl =
+  "https://api.open-meteo.com/v1/forecast?latitude=47.8517&longitude=35.1171&current_weather=true&timezone=auto";
 
-console.log('before timeoutv');
+fetch(weatherUrl)
+  .then((response) => response.json())
+  .then((data) => generateWeather(data))
+  .catch((err) => console.log("error :>> ", err));
 
-setTimeout(() => {
-  console.log('in timeout'); // => webApi
-}, 1000);
+// Example: відобразити на сторінці поточну температуру з одиницею виміру
+// відобразити темп. від'ємну синім кольором, 0 -чорним
+//                   додатню до 40 - зеленим, >=40 - червоним
 
-console.log('after timeout');
+// Task: відобразити швидкість вітру з одиницею виміру
 
+function generateWeather({
+  current_weather: { temperature, windspeed },
+  current_weather_units: { temperature: tempUnit, windspeed: windUnit },
+}) {
+  const currentTemperatureEl = document.createElement("div");
+  currentTemperatureEl.textContent = `${temperature} ${tempUnit}`;
+  currentTemperatureEl.style.color = calcTemperatureColor(temperature);
 
-// call stack
-function f1() {
-  console.trace();
-  console.log('end');
+  const currentWindspeed = document.createElement("div");
+  currentWindspeed.textContent = `${windspeed} ${windUnit}`;
+  document.body.append(currentTemperatureEl, currentWindspeed);
 }
-function f() {
-  console.trace();
-  f1();
-}
 
-f();
+function calcTemperatureColor(temperature) {
+  if (temperature < 0) {
+    return "blue";
+  } else if (temperature === 0) {
+    return "black";
+  } else if (temperature > 0 && temperature < 40) {
+    return "green";
+  } else {
+    return "red";
+  }
+}
