c58b570805f9bd9ceb0de24c7b4734369a613fd6
Termins55 <kevgame123@gmail.com>
2025-09-02 20:47:42 +0300

    Add tempUnit switcher v1/ Refactor weather

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,36 @@
 "use strict";
 
-const weatherUrl =
-  "https://api.open-meteo.com/v1/forecast?latitude=47.8517&longitude=35.1171&current_weather=true&timezone=auto";
+let isCelsiiDegrii = true;
 
-fetch(weatherUrl)
-  .then((response) => response.json())
-  .then((data) => generateWeather(data))
-  .catch((err) => console.log("error :>> ", err));
+const tempUnitBtn = document.getElementById("tempUnitBtn");
+
+tempUnitBtn.onclick = switchTemperatureUnit;
+
+function switchTemperatureUnit() {
+  // поміняти значення прапорця на протилежне
+  isCelsiiDegrii = !isCelsiiDegrii;
+  // // поміняти напис на кнопці
+  // tempUnitBtn.textContent = `Switch to ${isCelsiiDegrii ? "F" : "C"}`;
+  // // завантажити дані з температурою в нових одиницях
+  // fetch()
+  //   .then((response) => response.json())
+  //   .then((data) => generateWeather(data))
+  //   .catch((err) => console.log("error :>> ", err));
+  updateData();
+}
+updateData();
+
+function updateData() {
+  tempUnitBtn.textContent = `Switch to ${isCelsiiDegrii ? "F" : "C"}`;
+  const weatherUrl = `https://api.open-meteo.com/v1/forecast?latitude=47.8517&longitude=35.1171&current_weather=true&timezone=auto${
+    isCelsiiDegrii ? "" : "&temperature_unit=fahrenheit"
+  }`;
+
+  fetch(weatherUrl)
+    .then((response) => response.json())
+    .then((data) => generateWeather(data))
+    .catch((err) => console.log("error :>> ", err));
+}
 
 // Example: відобразити на сторінці поточну температуру з одиницею виміру
 // відобразити темп. від'ємну синім кольором, 0 -чорним
@@ -18,13 +42,12 @@ function generateWeather({
   current_weather: { temperature, windspeed },
   current_weather_units: { temperature: tempUnit, windspeed: windUnit },
 }) {
-  const currentTemperatureEl = document.createElement("div");
+  const currentTemperatureEl = document.querySelector(".temp");
   currentTemperatureEl.textContent = `${temperature} ${tempUnit}`;
   currentTemperatureEl.style.color = calcTemperatureColor(temperature);
 
-  const currentWindspeed = document.createElement("div");
+  const currentWindspeed = document.querySelector(".wind");
   currentWindspeed.textContent = `${windspeed} ${windUnit}`;
-  document.body.append(currentTemperatureEl, currentWindspeed);
 }
 
 function calcTemperatureColor(temperature) {
